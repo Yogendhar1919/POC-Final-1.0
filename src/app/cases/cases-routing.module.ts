f8decf2844180689bdca71eac79c5ee8c30e04d2
Yogendhar1919 <67677715+Yogendhar1919@users.noreply.github.com>
2022-09-02 13:24:08 -0500

    1 file modified

--- src/app/cases/cases-routing.module.ts
+++ src/app/cases/cases-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from '@nativescript/angular';
 import { CasesListComponent } from './cases-list.component';
 import { CropListComponent } from './crop-list/crop-list.component';
+import { ARLineListComponent } from './arline-list/arline-list.component';
 
 const routes: Routes = [
   { path: '', component: CasesListComponent },
   {
     path: 'crop-list/:caseId',
     component: CropListComponent,
+  },
+  {
+    path: 'arline-list/:claimId/:policyCropId/:cropId/:countyId',
+    component: ARLineListComponent,
   },
 ];
 
